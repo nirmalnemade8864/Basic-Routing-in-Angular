dd3f93092e5b292d00eff866ef8e78d23dfc5cb4
Nirmal@6798 <nirmalnemade8864@gmail.com>
2020-09-30 16:06:29 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DemoComponent } from './demo/demo.component'
+import { InfoComponent } from './info/info.component'
+import { HeaderComponent } from './header/header.component'
+import { UserComponent } from './user/user.component'
 
-const routes: Routes = [];
+
+
+
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+
+
+
+  {
+    path: "demo",
+    component: DemoComponent
+  },
+  {
+    path: "info",
+    component: InfoComponent
+  },
+  {
+    path: "header",
+    component: HeaderComponent
+  },
+  {
+    path: "user",
+    component: UserComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+
+import { DemoComponent } from './demo/demo.component';
+import { InfoComponent } from './info/info.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+
+    DemoComponent,
+    InfoComponent,
+    HeaderComponent,
+    UserComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
